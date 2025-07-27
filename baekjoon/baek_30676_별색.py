e66cbdcf4120208102f9dc4f0f0bae8029f240dd
YeRimmm-Cho <rim000514@gmail.com>
2025-07-27 14:34:41 +0900

    solve: baek_3076 별 색깔 스펙트럼

---
+++ baekjoon/baek_30676_별색.py
@@ -0,0 +1,16 @@
+spectrum = int(input())
+
+if 620 <= spectrum <= 780:
+    print('Red')
+elif 590 <= spectrum < 620:
+    print('Orange')
+elif 570 <= spectrum < 590:
+    print('Yellow')
+elif 495 <= spectrum < 570:
+    print('Green')
+elif 450 <= spectrum < 495:
+    print('Blue')
+elif 425 <= spectrum < 450:
+    print('Indigo')
+elif 380 <= spectrum < 425:
+    print('Violet')
